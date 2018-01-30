63c9d665aadaf122cddac6ed77407ce4b3c0f5f6
Your Name <you@example.com>
2018-01-30 11:58:46 +0530

    done user authentication to view posts available

--- userblog/views.py
+++ userblog/views.py
@@ -31,6 +31,7 @@ def home(request):
 def login(request):
     return render(request, 'userblog/login.html')
 
+@login_required
 def userpost_list(request):
     posts=UserPost.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'userblog/userpost_list.html',{'posts':posts})
